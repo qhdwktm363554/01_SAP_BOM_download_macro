d82fdfa7228041166e3348496e5407e201be275c
qhdwktm363554 <qhdwktm3635@gmail.com>
2022-06-03 17:14:32 +0900

    files are deleted

--- bongasonasdfj.py
+++
@@ -1,9 +0,0 @@
-import bong
-from bong import *
-
-# bong.BOM_down()
-# a = bong.BOM_down()
-# print(a)
-
-hees()
-print(bong.heesy)
--- what_the_hell.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-
-d = pyautogui.locateCenterOnScreen('D:\\09.python\\06.PYAUTOGUI_SAP_BOM_download\\d_Filename.PNG')
-print(d)
-pyautogui.moveTo(d)
